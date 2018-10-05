294b10bbf5b76c73b8d5401a1e414f37794dad57
sandeeplunawath <sapphirej16@gmail.com>
2018-10-05 15:36:09 +0530

    basic script

---
+++ src/main/java/maven/test/webdriver/webdriver/SampleTest.java
@@ -0,0 +1,10 @@
+package maven.test.webdriver.webdriver;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sample hellow wordl");
+	}
+
+}
